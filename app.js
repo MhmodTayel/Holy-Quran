a30f9d7bd9dd0229650826b8b035cf0df88ebdef
MhmodTayel <mhmodtayel0050@gmail.com>
2021-01-13 03:09:06 +0200

    get surah by number

--- app.js
+++ app.js
@@ -2,17 +2,25 @@ const output = document.getElementById('output')
 const surah = document.getElementById('surah')
 const getSurah = document.getElementById('get-surah')
 getSurah.addEventListener('click',() => {
+  const surahNumber = surah.value
+  if(surahNumber < 115 && surahNumber > 0)
+   {  
+     output.innerText = ''
+     getSura(surahNumber)
+   } else {
+     output.innerText = 'ضع رقم صحيح للصوره '
+   }
 
 })
-async function getSura() {
+async function getSura(number) {
   let res = await
-   fetch(`https://api.quran.sutanlab.id/surah/${surah.value}`)
+   fetch(`https://api.quran.sutanlab.id/surah/${number? number :1}`)
   let output = await res.json()
    console.log(output)
    showSura(output)
  }
  
- getSura()
+
 
  function showSura(API_Output) {
    const verses = API_Output.data.verses
